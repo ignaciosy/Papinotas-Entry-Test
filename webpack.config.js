4d995199433f8f473563a3485a1d913420eaa60c
ignaciosy <ignaciosy0@gmail.com>
2019-09-02 16:15:21 -0400

    Add postcss and HtmlWebpackPlugin as requirements

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
-  entry: "./src/index.js",
-  output: { path: __dirname + "/dist", publicPath: "/", filename: "bundle.js" },
-  devServer: { contentBase: "./dist" },
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: { contentBase: "./build" },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader, 'eslint-loader'"],
+        use: ["babel-loader", "eslint-loader"]
       },
-    ],
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[path]___[name]__[local]___[hash:base64:5]"
+            }
+          },
+          { loader: "postcss-loader" }
+        ]
+      }
+    ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html")
+    })
+  ]
 };
