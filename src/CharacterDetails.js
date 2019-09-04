a97e2016fdbe74c3d89ca7128231fdcd1e878d00
ignaciosy <ignaciosy0@gmail.com>
2019-09-04 03:42:33 -0400

    Show detailed view for character as a Card from material-ui

--- src/CharacterDetails.js
+++ src/CharacterDetails.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
-import Avatar from "@material-ui/core/Avatar";
 import Chip from "@material-ui/core/Chip";
 import FaceIcon from "@material-ui/icons/Face";
-import DoneIcon from "@material-ui/icons/Done";
+import FavoriteBorderRoundedIcon from "@material-ui/icons/FavoriteBorderRounded";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
 
 const styles = {
   root: {
@@ -13,7 +14,11 @@ const styles = {
     flexWrap: "wrap"
   },
   chip: {
-    //margin: theme.spacing(1)
+    margin: ".2rem"
+  },
+  card: {
+    backgroundColor: "#7AEFFF",
+    margin: ".2rem"
   }
 };
 
@@ -43,43 +48,41 @@ class CharacterDetails extends React.Component {
     let classes = this.props.classes;
     if (this.state.isDetailedView) {
       return (
-        <div>
-          <a href="#" onClick={this.handleCharacterClick}>
-            <h2>{this.props.character.name}</h2>
-          </a>
-          <p>
-            <b>Homeworld:</b> {this.props.character.homeworld.name}
-          </p>
-          {this.props.character.species.map(specie => (
+        <Card className={classes.card}>
+          <CardActionArea onClick={this.handleCharacterClick}>
+            <Chip
+              icon={<FaceIcon />}
+              label={this.props.character.name}
+              className={classes.chip}
+              onClick={this.handleCharacterClick}
+              onDelete={this.handleFavouriteButtonClick}
+              color={this.props.isFavourite ? "secondary" : "primary"}
+              deleteIcon={<FavoriteBorderRoundedIcon />}
+              variant="outlined"
+            />
             <p>
-              <b>Species: </b>
-              {specie.name}
+              <b>Homeworld:</b> {this.props.character.homeworld.name}
             </p>
-          ))}
-          <b>Vehicles:</b>
-          <ul>
-            {this.props.character.vehicles.map(vehicle => (
-              <li>{vehicle.name}</li>
+            {this.props.character.species.map(specie => (
+              <p>
+                <b>Species: </b>
+                {specie.name}
+              </p>
             ))}
-          </ul>
-          <b>Films:</b>
-          <ul>
-            {this.props.character.films.map(film => (
-              <li>{film.title}</li>
-            ))}
-          </ul>
-          <button
-            type="button"
-            style={
-              this.props.isFavourite
-                ? { background: "yellow" }
-                : { background: "green" }
-            }
-            onClick={this.handleFavouriteButtonClick}
-          >
-            Fav
-          </button>
-        </div>
+            <b>Vehicles:</b>
+            <ul>
+              {this.props.character.vehicles.map(vehicle => (
+                <li>{vehicle.name}</li>
+              ))}
+            </ul>
+            <b>Films:</b>
+            <ul>
+              {this.props.character.films.map(film => (
+                <li>{film.title}</li>
+              ))}
+            </ul>
+          </CardActionArea>
+        </Card>
       );
     } else {
       return (
@@ -91,7 +94,7 @@ class CharacterDetails extends React.Component {
             onClick={this.handleCharacterClick}
             onDelete={this.handleFavouriteButtonClick}
             color={this.props.isFavourite ? "secondary" : "primary"}
-            deleteIcon={<DoneIcon />}
+            deleteIcon={<FavoriteBorderRoundedIcon />}
             variant="outlined"
           />
         </div>
